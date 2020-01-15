9cef1c527667891b82242e6aa7b076a86534d2fc
JIBaker92 <j_i_baker@yahoo.co.uk>
2020-01-15 00:40:50 +0000

    Third commit, login screen coded

--- app/src/main/java/jimmy/example/educationabsenceapplication/MainActivity.java
+++ app/src/main/java/jimmy/example/educationabsenceapplication/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,12 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
+
     }
 
     @Override
     protected void onStart(){
         super.onStart();
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(currentUser == null){
+
+            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
+            finish();
+
+        }
     }
 }
